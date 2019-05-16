fd575941b03c793aba5d5c0849d03e1226a34ad7
Danil Filchenkov <nerdy.usa@gmail.com>
2019-05-16 22:12:52 +0300

    Fixed the number of digits

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 // nerdyfeed 2019
 function calc() {
-    var a = parseFloat(document.getElementById('first').value); // Получение первого числа
-    var b = parseFloat(document.getElementById('second').value); // Получение второго числа
+    var a = document.getElementById('first').value // Получение первого числа
+    var b = document.getElementById('second').value // Получение второго числа
+    a = parseFloat(a);
+    b = parseFloat(b);
     var c = a + b; // Производим вычисления
-    alert("Результат: \n\n" + Math.round(c * 100) / 100); // Вывод результата с учётом округления
+    if(c <= 0.9) {
+        alert("Результат: \n\n" + Math.round(c * 100) / 100); // Вывод результата с учётом округления
+    } else {
+        alert("Результат: \n\n" + c);// Вывод результата с учётом округления
+    }
+    
 }
 // Анонимная функция для кнопки
 result.onclick = function() {
